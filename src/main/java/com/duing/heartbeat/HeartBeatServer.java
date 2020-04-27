56202e724c8f6dc62768b25a9b0dc70a79fb589e
Demi91 <lanluo_bingzi@126.com>
2020-04-27 22:11:36 +0800

    day7

---
+++ src/main/java/com/duing/heartbeat/HeartBeatServer.java
@@ -0,0 +1,74 @@
+package com.duing.heartbeat;
+
+import com.duing.websocket.WebSocketInitializer;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import io.netty.handler.timeout.IdleStateHandler;
+
+import java.util.concurrent.TimeUnit;
+
+public class HeartBeatServer {
+
+    int port;
+
+    public HeartBeatServer(int port) {
+        this.port = port;
+    }
+
+    public void start() {
+
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .childHandler(new HeartBeatInitializer());
+
+            // 启动
+            ChannelFuture channelFuture = serverBootstrap.bind(port).sync();
+            channelFuture.channel().closeFuture().sync();
+
+        } catch (Exception e) {
+
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+
+
+    static class HeartBeatInitializer extends ChannelInitializer<Channel>{
+
+        @Override
+        protected void initChannel(Channel ch) throws Exception {
+            ChannelPipeline pipeline = ch.pipeline();
+
+            // 使用字符串的编码解码器
+            pipeline.addLast(new StringDecoder());
+            pipeline.addLast(new StringEncoder());
+
+            // 使用空闲状态处理器
+            //  检测的是 IdleStateEvent事件  通过管道传递给下一个handler处理
+            //  userEventTriggered
+            pipeline.addLast(new IdleStateHandler(2,3,5, TimeUnit.SECONDS));
+
+            pipeline.addLast(new HeartBeatHandler());
+        }
+    }
+
+
+    public static void main(String[] args) {
+        HeartBeatServer server = new HeartBeatServer(9999);
+        server.start();
+    }
+}
---
+++ src/main/java/com/duing/tcp/TimeServer.java
@@ -0,0 +1,80 @@
+package com.duing.tcp;
+
+import com.duing.heartbeat.HeartBeatServer;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+import java.util.Date;
+
+public class TimeServer {
+
+    public static void main(String[] args) {
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .option(ChannelOption.SO_BACKLOG, 128)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(null);
+                        }
+                    });
+
+            // 启动
+            ChannelFuture channelFuture = serverBootstrap.bind(0101).sync();
+            channelFuture.channel().closeFuture().sync();
+
+        } catch (Exception e) {
+
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+
+
+    static class TimeServerHandler extends ChannelInboundHandlerAdapter {
+
+        // 请求次数计数
+        int count;
+
+        @Override
+        public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+            ByteBuf buf = (ByteBuf) msg;
+            // 声明数组接收其内容
+            byte[] req = new byte[buf.readableBytes()];
+            buf.readBytes(req);
+
+            // 请求的长度 - 系统分隔符的长度 = 数据的长度
+            // 将数组转为字符串后  截取
+            // System.getProperty("line.separator") 代表系统所支持的分隔符
+            //  windows和linux支持的不同
+            String data = new String(req, "UTF-8").substring(0, req.length -
+                    System.getProperty("line.separator").length());
+
+            String timeStr = new Date().toString();
+            String currentTime = "Query Data :" + data + "; current time" + timeStr
+                    + "; count is" + ++count;
+
+            System.out.println(currentTime);
+            ByteBuf resp = Unpooled.copiedBuffer(currentTime.getBytes());
+            ctx.writeAndFlush(resp);
+
+        }
+    }
+}
